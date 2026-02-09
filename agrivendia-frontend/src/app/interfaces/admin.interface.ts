fb342d6749e5330888908a8d6997e3b10e520af1
Thanmai <test@email.com>
2026-02-09 15:21:19 +0000

    removed comment from product_buy

--- agrivendia-frontend/src/app/interfaces/admin.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface Admin {
-    id: number;
-    name: string;
-    email: string;
-    phone: string;
-    role: 'admin';
-}
--- agrivendia-frontend/src/app/interfaces/customer.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface Customer {
-    id: number;
-    name: string;
-    email: string;
-    phone: string;
-    address: string;
-    profile_image?: string;
-}
